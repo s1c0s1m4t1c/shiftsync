f57938fdbe35d17e3bc377490f2893bef1bf3594
s1c0s1m4t1c <152750760+s1c0s1m4t1c@users.noreply.github.com>
2026-06-15 14:41:28 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './' // ADD THIS LINE: It tells Vite to look for files relative to the current path
 })
